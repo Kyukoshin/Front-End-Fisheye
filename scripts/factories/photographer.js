16b4ec0a8edbc2ca3267071bee7bcb7023ac77c5
RemiBertrand <rbertrand21@gmail.com>
2023-05-24 09:40:30 +0200

    Sorting done

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -16,13 +16,14 @@ function photographerFactory(data) {
         const h4 = document.createElement( 'h4' );
         h4.textContent = tagline;
         const h5 = document.createElement( 'h5' );
-        h5.textContent = price+"€/jour";
+        h5.textContent = price+"€/jour";        
         article.appendChild(link);
         link.appendChild(img);
         link.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
-        article.appendChild(h5);
+        article.appendChild(h5); 
+
         return (article);
     }
     return { name, portrait, tagline, price, city, country, getUserCardDOM }
@@ -47,9 +48,10 @@ function photographerSingleFactory(data) {
         const h4 = document.createElement( 'h4' );
         h4.textContent = tagline;
         const h5 = document.createElement( 'h5' );
-        h5.textContent = price+"€/jour";     
+        h5.textContent = price+"€ / jour";     
         const totLikes = document.createElement( 'h5' );
-        totLikes.textContent = totalLikes;   
+        totLikes.textContent = totalLikes;  
+        totLikes.setAttribute("id","totLikes") ;
         const totHeart = document.createElement( 'i' );
         totHeart.setAttribute("class","fa-solid fa-heart");
         const modal = document.createElement( 'button' );
@@ -72,7 +74,7 @@ function photographerSingleFactory(data) {
         article.appendChild(info);
         article.appendChild(modale);
         article.appendChild(portrait);
-        //article.appendChild(h5);
+
         return (article);
     }
     return { name, portrait, tagline, price, city, country, getUserCardDOM }
@@ -81,7 +83,7 @@ function photographerSingleFactory(data) {
 let numSlide = 1
 
 function photographerMediaFactory(data) {
-    const { date,likes,price,title,video,image,id } = data;
+    const {date,likes,price,title,video,image,id} = data;
 
     const picture = `D:\\Projets_Code\\OC\\Projet_FishEye\\Front-End-Fisheye-main\\assets\\photographers\\Sample Photos\\PhotographersPhotos\\`+image;
 
@@ -94,18 +96,75 @@ function photographerMediaFactory(data) {
         const desc = document.createElement( 'div' );
         desc.setAttribute("class", "picDesc")
         const heart = document.createElement( 'i' );
-        heart.setAttribute("class","fa-solid fa-heart");
+        heart.setAttribute("class","fa-regular fa-heart");
         heart.setAttribute("style", "color: #901c1c");
         h4.appendChild(heart);
-        article.appendChild(new factoryImage(data));        
+
+        //Compteur likes
+        heart.addEventListener("click", () => {
+            if(heart.classList.contains("liked")){
+            let newLikes = this.likes
+            totalLikes--
+            totLikes=document.getElementById("totLikes")
+            totLikes.textContent = totalLikes; 
+            h4.textContent = newLikes+" "
+            h4.appendChild(heart)
+            heart.classList.remove("liked")
+            heart.classList.remove("fa-solid")
+            heart.classList.add("fa-regular")
+           
+            }
+            else{
+            let newLikes = this.likes
+            newLikes++
+            totalLikes++
+            totLikes=document.getElementById("totLikes")
+            totLikes.textContent = totalLikes; 
+            h4.textContent = newLikes+" "
+            h4.appendChild(heart)
+            heart.classList.add("liked")
+            heart.classList.add("fa-solid")
+            heart.classList.remove("fa-regular")
+            
+            }
+        });
+        
+        const articleImage = document.createElement('a');
+        articleImage.appendChild(new factoryImage(data)); 
+        article.appendChild(articleImage);       
         desc.appendChild(h3);
         desc.appendChild(h4);
         article.appendChild(desc);  
-        article.setAttribute("onclick",`displayLightBox();currentSlide(${numSlide})`); 
+        articleImage.setAttribute("onclick",`displayLightBox();currentSlide(${numSlide})`); 
         numSlide ++     
         return (article);
     }
     return { date,likes,price,title,video,image,id, getUserCardDOM }
+}
+
+function sortFactory(){
+    
+    const anchorFilter = document.createElement('div');
+    const sortText = document.createElement('p');
+    sortText.innerHTML = "Trier par";
+    const sortMenu = document.createElement('ul');        
+    const sortPop = document.createElement('li');
+    sortPop.innerHTML= "Popularité";
+    sortPop.setAttribute("onclick", `sortBy("popularity")`);
+    const sortDate = document.createElement('li');
+    sortDate.innerHTML= "Date";
+    sortDate.setAttribute("onclick", `sortBy("date")`);
+    const sortTitle = document.createElement('li');
+    sortTitle.innerHTML= "Titre";
+    sortTitle.setAttribute("onclick", `sortBy("title")`);
+    sortMenu.appendChild(sortPop);
+    sortMenu.appendChild(sortDate);
+    sortMenu.appendChild(sortTitle);
+    anchorFilter.appendChild(sortText);
+    anchorFilter.appendChild(sortMenu);
+
+    return anchorFilter
+
 }
 
 function lightboxFactory(media) {
@@ -119,6 +178,8 @@ function lightboxFactory(media) {
         caption.setAttribute("class","lightboxPicDesc");
         caption.innerHTML = title;
         img.appendChild(caption); 
+        totalLikes += likes
+        console.log(totalLikes)
         return (img);
     }
     return { date,likes,price,title,video,image,id, getUserCardDOM }
@@ -145,4 +206,16 @@ class factoryImage {
         img.src= `D:\\Projets_Code\\OC\\Projet_FishEye\\Front-End-Fisheye-main\\assets\\photographers\\Sample Photos\\PhotographersPhotos\\`+image
         return img
     }
+}
+
+function sortBy(n) {
+    if (n === "popularity") {
+        getPhotographers("likes")
+    }
+    else if (n === "date") {
+        getPhotographers("date")
+    }
+    else if (n === "title") {
+        getPhotographers("title")    
+    }        
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,7 +4,7 @@ console.log(id);
 let currentPhotographer;
 let totalLikes = 0;
 
-async function getPhotographers() {
+async function getPhotographers(filter) {
     fetch('https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Front-End-Fisheye/main/data/photographers.json')
     .then((response) => {
         if (response.ok) {
@@ -13,11 +13,46 @@ async function getPhotographers() {
             console.log(`Erreur : ${response.status}`);
         }
     })
-    .then((data) => extractData(data))
+    .then((data) => extractData(data, filter))
 }
 
-function extractData(data) {
-  console.log(data);
+function extractData(data, sortProperty) {  
+  let a = data.media.filter((media)=>(media.photographerId===Number(id)))
+  console.log(sortProperty)
+
+  let sortedMedia = a.sort((a, b) => {
+    // use comparison tu get the filtered array
+    if (sortProperty === "likes" || sortProperty === "date") {
+      return b[sortProperty] - a[sortProperty];
+    } else if (sortProperty === "title") { //use localcompare to compare string values
+      return a[sortProperty].localeCompare(b[sortProperty]);
+    }
+  });
+  
+  //clean HTML
+  const anchorMedia = document.querySelector(".photograph-content")
+  removeAllChildNodes(anchorMedia)
+  const anchorPhotographer = document.querySelector(".photograph-header")
+  removeAllChildNodes(anchorPhotographer)
+  const anchorSort = document.querySelector(".photograph-sortMenu")
+  removeAllChildNodes(anchorSort)
+
+  //photographer pictures
+  sortedMedia.forEach((media) => {
+    //fetch only media where link id is found and inject in dedicated factory     
+      const photographersSection = document.querySelector(".photograph-content");
+      const photographerModel = photographerMediaFactory(media);
+      const userCardDOM = photographerModel.getUserCardDOM();      
+      photographersSection.appendChild(userCardDOM);
+
+      //lightbox
+      const lightboxSection = document.querySelector(".lightboxContainer");
+      const lightboxBuilder = lightboxFactory(media);
+      const lightboxDOM = lightboxBuilder.getUserCardDOM();            
+      lightboxSection.appendChild(lightboxDOM); 
+  });
+  
+  //photographer header
   data.photographers.forEach((photographer) => {
     //fetch only photographer where link id is found and inject in dedicated factory
     if (photographer.id === Number(id)) {
@@ -27,26 +62,18 @@ function extractData(data) {
       photographersSection.appendChild(userCardDOM);       
     }
   });
-  let a = data.media.filter((media)=>(media.photographerId===Number(id)))
-  console.log(a)
-  a.forEach((media) => {
-    //fetch only media where link id is found and inject in dedicated factory     
-      const photographersSection = document.querySelector(".photograph-content");
-      const photographerModel = photographerMediaFactory(media);
-      const userCardDOM = photographerModel.getUserCardDOM();
-      photographersSection.appendChild(userCardDOM);      
-      
-      const lightboxSection = document.querySelector(".lightboxContainer");
-      const lightboxBuilder = lightboxFactory(media);
-      const lightboxDOM = lightboxBuilder.getUserCardDOM();
-      lightboxSection.appendChild(lightboxDOM); 
-  });
+
+  //sorting div
+  const sortSection = document.querySelector(".photograph-sortMenu");
+  const sortDiv = sortFactory();
+  sortSection.appendChild(sortDiv);  
+
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
 }
 
 getPhotographers();
-
-
-//onlick sur le foreach
-//afficher galerie sur le onclick de l'image
-//let a = data.media.filter((media)=>(media.photographerId===Number(id)))
-//tableau.indexof()
